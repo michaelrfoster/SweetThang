32bcfb0bfcbda1996e92edc31a04481efd5836ff
MichaelFoster <michaelrfoster0@gmail.com>
2019-09-28 15:15:12 -0400

    All files initially
    Doesn't have completed Calculator

---
+++ Assets/Scenes/Scripts/ClickScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickScript : MonoBehaviour
+{
+    public Text UIText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var inc = int.Parse(UIText.text) + 1;
+        UIText.text = inc.ToString();
+        Debug.Log(UIText.text);
+    }
+
+
+
+    public void increment()
+    {
+        Debug.Log("Yo you suck boy");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
