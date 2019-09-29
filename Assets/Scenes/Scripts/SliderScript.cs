954204070da794d869e335127273d35e7e6e00e8
ThatEvilChickenNextDoor <kwzeyunwang@gmail.com>
2019-09-28 21:06:26 -0400

    slide

---
+++ Assets/Scenes/Scripts/SliderScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour
+{
+    public GameObject cube;
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void UpdateSize()
+    {
+        cube.gameObject.transform.localScale = new Vector3(slider.value, slider.value, slider.value);
+    }
+}
