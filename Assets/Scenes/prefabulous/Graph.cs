0cfff5532df45f0663cb395ca7292814387d210b
ThatEvilChickenNextDoor <kwzeyunwang@gmail.com>
2019-09-28 20:02:18 -0400

    Sine wave
    It sines

--- Assets/Scenes/prefabulous/Graph.cs
+++ Assets/Scenes/prefabulous/Graph.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Graph : MonoBehaviour
 {
     public Transform pointPrefab;
+
+    [Range(10, 100)]
+    public int resolution = 10;
+
+    Transform[] points;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,21 +19,31 @@ public class Graph : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            point.localPosition = position;
+        }
     }
 
     void Awake()
     {
-        Vector3 scale = Vector3.one / 5f;
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
         Vector3 position;
+        points = new Transform[resolution];
         position.y = 0f;
         position.z = 0f;
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < resolution; i++)
         {
             Transform point = Instantiate(pointPrefab);
-            position.x = ((i + 0.5f) / 5f) - 1f;
+            points[i] = point;
+            position.x = (i + 0.5f) * step - 1f;
             point.localPosition = position;
             point.localScale = scale;
+            point.SetParent(transform,false);
         }
     }
 }
